256b7733e386d4a3fc65ac54363dae6413e91ef3
Dmytro Kolesnykov <kastaneda@gmail.com>
2024-11-25 09:14:09 +0200

    Refactoring

--- just_mqtt/main.py
+++ just_mqtt/main.py
@@ -5,50 +5,26 @@ def topic(suffix):
     return (config.board_id + '/' + suffix).encode('ascii')
 
 connect.loadRTC()
-shift_stepper.loadPosition(connect.rtcm.get('motor'))
 connect.setupWifi()
-
-# https://github.com/micropython/micropython-lib/blob/master/micropython/umqtt.simple/README.rst
-# keepalive=1 is VERY small period; for real life it should be >= 5 sec
-client = umqtt.robust.MQTTClient(config.board_id, connect.wifi('mqtt'), keepalive=1)
-client.set_last_will(topic('status'), b'0')
-client.connect()
-client.publish(topic('status'), b'1')
 
 vcc = machine.ADC(1)
 
 led = machine.Pin(2, machine.Pin.OUT)
 led.value(1)
 
+shift_stepper.loadPosition(connect.rtcm.get('motor'))
+# The only one background job that really needs timer
+stepper_timer = machine.Timer(-1)
+stepper_timer.init(
+    callback=lambda t: shift_stepper.oneStep(),
+    freq=400,
+    mode=machine.Timer.PERIODIC
+)
+
 # The onboard LED is inverted: value(0) means ON, 1 means OFF
 # It's more practical to invert it here, on the device's side
 def report_led():
     client.publish(topic('led'), str(1-led.value()))
-
-async def led_blink(times):
-    for i in range(times):
-        led.value(0)
-        report_led()
-        await asyncio.sleep_ms(250)
-        led.value(1)
-        report_led()
-        await asyncio.sleep_ms(250)
-
-topic_callbacks = {
-    topic('led/set'): lambda m: led.value(0 if m == b'1' else 1),
-    topic('motor1/set'): shift_stepper.motor1.target,
-    topic('motor2/set'): shift_stepper.motor2.target,
-    topic('motor3/set'): shift_stepper.motor3.target,
-    topic('sleep/set'): connect.deepSleep,
-    topic('led_blink/set'): lambda m: asyncio.create_task(led_blink(int(m)))
-}
-
-def mqtt_callback(topicName, msg):
-    fn = topic_callbacks.get(topicName, lambda x: None)
-    fn(msg)
-
-client.set_callback(mqtt_callback)
-client.subscribe(topic('+/set'))
 
 recent_reports = {}
 def report_changed(name, value):
@@ -81,30 +57,51 @@ async def every20s():
         client.publish(topic('motor3'), str(shift_stepper.motor3.stepActual))
         await asyncio.sleep_ms(20_000)
 
-stepper_timer = machine.Timer(-1)
-stepper_timer.init(
-    callback=lambda t: shift_stepper.oneStep(),
-    freq=400,
-    mode=machine.Timer.PERIODIC
-)
-
 async def mqtt_loop():
     while True:
         client.check_msg()
         await asyncio.sleep_ms(50)
 
+async def led_blink(times):
+    for i in range(times * 2):
+        led.value(1 - led.value())
+        report_led()
+        await asyncio.sleep_ms(250)
+
+topic_callbacks = {
+    topic('led/set'): lambda m: led.value(0 if m == b'1' else 1),
+    topic('motor1/set'): shift_stepper.motor1.target,
+    topic('motor2/set'): shift_stepper.motor2.target,
+    topic('motor3/set'): shift_stepper.motor3.target,
+    topic('sleep/set'): connect.deepSleep,
+    topic('led_blink/set'): lambda m: asyncio.create_task(led_blink(int(m)))
+}
+
+def mqtt_callback(topicName, msg):
+    fn = topic_callbacks.get(topicName, lambda x: None)
+    fn(msg)
+
+# https://github.com/micropython/micropython-lib/blob/master/micropython/umqtt.simple/README.rst
+# keepalive=1 is VERY small period; for real life it should be >= 5 sec
+client = umqtt.robust.MQTTClient(config.board_id, connect.wifi('mqtt'), keepalive=1)
+client.set_last_will(topic('status'), b'0')
+client.connect()
+client.publish(topic('status'), b'1')
+
+client.set_callback(mqtt_callback)
+client.subscribe(topic('+/set'))
+
 try:
     print('Start MQTT main loop, press Ctrl-C to stop')
-    asyncio.create_task(mqtt_loop())
     asyncio.create_task(every100ms())
     asyncio.create_task(every1s())
     asyncio.create_task(every20s())
+    asyncio.create_task(mqtt_loop())
     asyncio.get_event_loop().run_forever()
 except KeyboardInterrupt:
     print('\nStopped')
 finally:
-    client.disconnect()
-    asyncio.get_event_loop().stop()
     stepper_timer.deinit()
     connect.saveRTC()
-
+    client.disconnect()
+    asyncio.get_event_loop().close()
