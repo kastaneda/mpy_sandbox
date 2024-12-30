66412697216c8574b4226558b472a2d82dc69786
Dmytro Kolesnykov <kastaneda@gmail.com>
2024-12-30 23:49:03 +0200

    update

--- just_pep8/demo2/dsrv.py
+++ just_pep8/demo2/dsrv.py
@@ -2,23 +2,23 @@ import asyncio
 
 class MyDelayServo:
     def __init__(self):
-        self.event_update = asyncio.Event()
+        self.ev_update = asyncio.Event()
         self.delay = 1.5
         self.angle = 90
 
     def go(self, new_angle):
         if self.angle != new_angle:
             self.angle = new_angle
-            self.event_update.set()
+            self.ev_update.set()
 
     async def main(self):
         while True:
-            await self.event_update.wait() # to start updating
+            await self.ev_update.wait() # to start updating
             try:
-                self.event_update.clear()
+                self.ev_update.clear()
                 print('+', self.angle, end='') # sg90.init(freq=50, duty_u16=u16)
-                await asyncio.wait_for(self.event_update.wait(), self.delay)
+                await asyncio.wait_for(self.ev_update.wait(), self.delay)
             except asyncio.TimeoutError:
-                pass
+                pass # normal, uninterrupted operation
             finally:
                 print('-', end='') # sg90.deinit()
