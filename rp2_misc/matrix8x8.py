13235107d1a0febbe28a563e8043eda008f7ddf0
Dmytro Kolesnykov <kastaneda@gmail.com>
2025-05-20 22:46:58 +0300

    more randomness

--- rp2_misc/matrix8x8.py
+++ rp2_misc/matrix8x8.py
@@ -7,15 +7,15 @@ import colorsys
 pin = Pin(14, Pin.OUT)
 num_leds = 64
 np = neopixel.NeoPixel(pin, num_leds)
-dots = [(0,(0,0,0)) for i in range(12)]
+dots = [(0,(0,0,0)) for i in range(13)]
 
 try:
     while True:
-        time.sleep(0.05)
+        time.sleep(0.05 + random.random()**3 / 4)
         # a random pixel
         dot_addr = random.randrange(len(np))
-        rgb = colorsys.hsv_to_rgb(random.random(), 1, .1)
-        rgb_int = [int(color*256) for color in rgb]
+        rgb = colorsys.hsv_to_rgb(random.random(), 1, random.uniform(.01, .04))
+        rgb_int = [int(color*255) for color in rgb]
         dot = (dot_addr, rgb_int)
         dots = dots[1:] + [dot]
         print(dot)
