9189d731600619a980d71fd6d7d62687099cc1f2
Dmytro Kolesnykov <kastaneda@gmail.com>
2025-11-07 03:03:13 +0200

    Kerning

--- matchbox/ascii_banner.php
+++ matchbox/ascii_banner.php
@@ -3,17 +3,21 @@
 
 //$msg = 'Hello, world!';
 //$msg = 'strlen($msg);';
-$msg = 'Quick brown fox';
+//$msg = 'Quick brown fox';
+//$msg = 'jumps over the';
+$msg = 'Pixel craft';
 $font = require 'ascii.php';
 
 $banner = [];
+
+$kernFix = ['fo', 'ra', 'ft'];
 
 $prev = '';
 for ($i = 0; $i < strlen($msg); $i++) {
     $next = $msg[$i];
     $nextGlyph = $font[ord($msg[$i])];
     if (count($banner)) {
-        if ($prev == 'f' && $next == 'o') {
+        if (in_array($prev . $next, $kernFix)) {
             $banner = array_merge($banner, $nextGlyph);
         } else {
             $banner = array_merge($banner, [0], $nextGlyph);
