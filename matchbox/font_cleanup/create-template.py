f97f277cde6ae1cf89c632526f195ea274a9b048
Dmytro Kolesnykov <kastaneda@gmail.com>
2025-11-12 18:32:53 +0200

    hmm

--- matchbox/font_cleanup/create-template.py
+++ matchbox/font_cleanup/create-template.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
 
 from PIL import Image, ImageDraw
-from config import *
-
-dot_full_size = dot_size + dot_gap
-line_width = dot_full_size * line_dots_x
-line_height = dot_full_size * line_dots_y
-lines_height = line_height*lines + line_gap*(lines-1)
-img_width = img_padding*2 + line_width
-img_height = img_padding*2 + lines_height
+from grid import *
 
 img = Image.new('RGB', (img_width, img_height), color_bg)
 draw = ImageDraw.Draw(img)
 
 for line in range(lines):
-    line_start_x = img_padding
-    line_start_y = img_padding + (line_height+line_gap)*line
     for dot_x in range(line_dots_x):
-        x1 = line_start_x + dot_full_size*dot_x
-        x2 = x1 + dot_size - 1
         for dot_y in range(line_dots_y):
-            y1 = line_start_y + dot_full_size*dot_y
-            y2 = y1 + dot_size - 1
+            x1, y1 = dot_coord(line, dot_x, dot_y)
+            x2, y2 = x1+dot_size-1, y1+dot_size-1
             color = color_off
             if line == 0 and dot_x == 0 and dot_y == 0:
                 color = color_on
--- matchbox/font_cleanup/read-font.py
+++ matchbox/font_cleanup/read-font.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 
-print('TODO FIXME')
+from PIL import Image
+from config import *
+
+img = Image.open('font.png').convert('RGB')
+hex_color =  '#%02x%02x%02x' % img.getpixel((28, 20))[:3]
+print(hex_color)
